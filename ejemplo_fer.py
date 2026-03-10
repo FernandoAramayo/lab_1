78f4ededb4df5c0b7402d5e1f6a6184a79f5a03c
FernandoAramayo <fernando.aramayo.s@ucb.edu.bo>
2026-03-10 13:51:12 -0400

    capicua2

--- ejemplo_fer.py
+++ ejemplo_fer.py
@@ -1,6 +1,11 @@
 n = input("numero: ")
-n2 = n[::-1]
-if n == n2:
-    print("capicua")
-else:
-    print("no capicua")
+
+try:
+    int(n)
+    n2 = n[::-1]
+    if n == n2:
+        print("capicua")
+    else:
+        print("no capicua")
+except ValueError:
+    print("No es numero")
